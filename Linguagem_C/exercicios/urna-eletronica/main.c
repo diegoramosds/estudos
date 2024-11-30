55b23fe882be427448fe3220f42baede1fa3c720
Diego <diegoramos3709@gmail.com>
2024-11-30 15:29:51 -0300

    Corrigindo erros

--- Linguagem_C/exercicios/urna-eletronica/main.c
+++ Linguagem_C/exercicios/urna-eletronica/main.c
@@ -6,13 +6,12 @@ struct Candidato
     char nome[50];
     int votos;
 };
-
 int votar(struct Candidato candidatos[], int totalCandidatos)
 {
     int voto;
-    int encontrado = 0;
     printf("Digite o número do candidato (1 a 99): ");
     scanf("%d", &voto);
+    int encontrado = 0;
     for (int i = 0; i < totalCandidatos; i++)
     {
         if (candidatos[i].numero == voto)
@@ -23,13 +22,12 @@ int votar(struct Candidato candidatos[], int totalCandidatos)
             return 1;
         }
     }
-     if (!encontrado)
+    if (!encontrado)
     {
         printf("Número de candidato inválido!\n");
         return 0;
     }
-
-       return 0;
+    return 0;
 }
 void apurarVotos(struct Candidato candidatos[], int totalCandidatos)
 {
@@ -62,6 +60,7 @@ int main()
     do
     {
         printf("\nMenu de opções:\n");
+
         printf("1. Votar\n");
         printf("2. Apurar\n");
         printf("3. Emitir percentual de votos\n");
@@ -84,23 +83,7 @@ int main()
             break;
         default:
             printf("Opção inválida!\n");
-            break;
         }
     } while (opcao != 4);
     return 0;
 }
-
-// Mapa:
-
-// Entretanto, a equipe de testes está relatando quatro erros no programa, são eles:
-
-// 1 - Quando digitado um valor de número de candidato inválido, deveria aparecer a mensagem "Número de candidato inválido!”, e isso não está ocorrendo;
-// 2 - A opção apurar votos está mostrando as somas de votos de forma incorreta.
-// 3 - A opção emitir percentual de votos está errada, não está mostrando o percentual de votos por candidato de forma correta.
-// 4 - O menu está com algum problema ao selecionar a opção 1, 2 ou 3; as subsequentes também são executadas. 
-
-// (2) SEGUNDA ETAPA: Realização da atividade
-
-// Como programador, você deve identificar o trecho de código que está com problema. Após identificar, você deve fazer uma descrição relatando onde o problema ocorre (pode identificar a linha para ficar mais claro) e, depois, descreva a solução proposta, para que o programa funcione de forma correta.
-
-// Atenção: NÃO é para enviar o código corrigido, apenas a descrição da correção de cada um dos itens mencionados.
